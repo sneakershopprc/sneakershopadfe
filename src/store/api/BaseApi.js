8ee356167c80a1227efe7eda6b00613f5ee63fda
Thien Phuc <ttran.phuc99@gmail.com>
2020-10-20 16:33:11 +0700

    config

--- src/store/api/BaseApi.js
+++ src/store/api/BaseApi.js
@@ -1,5 +1,6 @@
 class Helper {
-  API_BASE = 'https://localhost:44303/api'
+  // API_BASE = 'https://localhost:44303/api'
+  API_BASE = 'https://sneakershopprc391.azurewebsites.net'
 
   getToken () {
     var user = JSON.parse(localStorage.getItem('user'))
